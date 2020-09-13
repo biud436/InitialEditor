d7476bc106204ef798b50057b3aa3d93577c9d74
biud436 <biud436@gmail.com>
2020-09-13 19:16:42 +0900

    메뉴 컨트롤 수정

--- js/component.js
+++ js/component.js
@@ -5,11 +5,27 @@ class Component {
     }
 
     initMembers(...args) {
+        this._isActiveEvent = false;
+    }
 
+    active() {
+        this._isActiveEvent = true;
+    }
+    
+    deactive() {
+        this._isActiveEvent = false;
+    }
+
+    isActiveEvent() {
+        return this._isActiveEvent;
     }
 
     start() {
         return this;
+    }
+
+    update(...args) {
+
     }
 }
 
--- js/index.js
+++ js/index.js
@@ -1,17 +1,93 @@
 // import {Component, BasicComponent} from "./component";
 import Renderer from "./renderer.js";
+import { Component } from "./component.js";
+
+class MenuComponent extends Component {
+    start() {
+        
+    }
+}
+
+class ActiveMenuComponent extends Component {
+    initMembers() {
+        this._count = 0;
+    }
+    start() {
+        this.active();
+    }
+    update(...args) {
+        const mouse = window.app._mouse;
+        const target = mouse.target;
+
+        if(target) {
+            if(target.tagName.toLowerCase() === "li") {
+                
+            }
+        }
+    }
+}
 
 class App {
 
     initMembers() {
         this.windowIds = {};
+        this._mouse = {
+            x: 0,
+            y: 0,
+            buttons: {
+                left: false,
+                leftFire: false,
+            },
+            target: null,
+        };
+        this._now = performance.now();
+        this._isMenuOpen = false;
+    }
+
+    initWithComponents() {
+        /**
+         * @type {Component[]}
+         */
+        this._components = [];
+        this._components.push(new MenuComponent());
+        this._components.push(new ActiveMenuComponent(true));
+        this._components.forEach(component => {
+            component.start();
+        })
+    }
+
+    initWithMouseEvent() {
+        window.addEventListener("mousemove", (ev) => {
+            this._mouse.x = ev.pageX;
+            this._mouse.y = ev.pageY;
+        }, false);
+        window.addEventListener("mousedown", (ev) => {
+            if(ev.button == 0) {
+                this._mouse.buttons.left = true;
+                this._mouse.buttons.leftFire = false;
+                this._mouse.target = ev.target;
+            }
+        }, false);
+        window.addEventListener("mouseup", (ev) => {
+            if(ev.button == 0) {
+                this._mouse.buttons.left = false;
+                this._mouse.buttons.leftFire = true;
+            }
+        }, false);
+        window.addEventListener("touchstart", (ev) => {
+            if(ev.button == 0) {
+                this._mouse.buttons.left = true;
+            }
+        }, false);        
+        window.addEventListener("touchend", (ev) => {
+            if(ev.button == 0) {
+                this._mouse.buttons.left = false;
+            }
+        }, false);        
     }
 
     initWithRenderer() {
         this._renderer = new Renderer({
-            // display: "relative",
-            // x: "50%",
-            // y: "50px",
             width: "100%",
             height: "100%",
             parentId: ".flex-container",
@@ -24,8 +100,9 @@ class App {
             .then(ret => {
                 this.windowIds["new-window"] = this._renderer;
                 document.querySelectorAll(".file-menu-new-button").forEach(i => {
-                    i.addEventListener("click", () => {
+                    i.addEventListener("click", (ev) => {
                         this._renderer.show();
+                        document.querySelector("#none").checked = true;
                     }, false);                    
                 })
             })
@@ -36,7 +113,49 @@ class App {
 
     start() {
         this.initMembers();
+        this.initWithMouseEvent();
+        this.initWithComponents();
         this.initWithRenderer();
+    }
+
+    update(deltaTime) {
+        if(deltaTime - this._now >= 400) {
+            this._now = deltaTime;
+        }
+
+        this.updateComponents();
+    }
+
+    updateComponents() {
+        const fire = this._mouse.buttons.leftFire;
+        const isLeft = this._mouse.buttons.left;
+        
+        this._components.forEach(component => {
+            if(!component.isActiveEvent()) {
+                component.update();
+            } else {
+                if(fire) {
+                    const target = this._mouse.target;
+
+                    if(target) {
+                        let parentNode = target.parentNode;
+                        while(parentNode != null && parentNode.className != "main") {
+                            parentNode = parentNode.parentNode;
+                        }
+                        if(parentNode && parentNode.className === "main") {
+                            this._isMenuOpen = true;
+                        } else {
+                            if(this._isMenuOpen && fire) {
+                                component.update();
+                                document.querySelector("#none").checked = true;
+                                this._isMenuOpen = false;
+                            }             
+                        }
+                    }
+                }
+                
+            }
+        });
     }
 
     onLoad(elem, id) {
@@ -45,7 +164,23 @@ class App {
             this.windowIds[id].onLoad(elem, self);
         }
     }
+
+    static GetInstance() {
+        if(!App.Instance) {
+            App.Instance = new App();
+        }
+        
+        return App.Instance;
+    }
 }
 
+App.Instance = null;
 window.app = new App();
 window.app.start();
+
+function update(deltaTime) {
+    window.app.update(deltaTime);
+    window.requestAnimationFrame(update);
+}
+
+update();
