e1c9a0e0c639c011ab93d437ffa13da620d34783
biud436 <biud436@gmail.com>
2020-10-02 20:27:21 +0900

    .

--- settings.bat
+++ settings.bat
@@ -1,8 +1,10 @@
-babel-polyfillnpm intall -y
+@echo off
+npm intall -y
 npm install -D webpack webpack-cli
-npm install --save-dev @babel/core @babel/cli
-npm install --save-dev @babel/preset-env
-npm install --save-dev @babel/polyfill
+REM npm install --save-dev @babel/core @babel/cli
+REM npm install --save-dev @babel/preset-env
+REM npm install --save-dev @babel/polyfill
 npm install --save-dev style-loader css-loader
 npm install --save-dev file-loader
 powershell wget "https://github.com/biud436/InitialEditor/raw/master/webpack.config.js" -OutFile "webpack.config.js"
+pause
