74cf62038c3132a2f3148c4c70b95627dffc671b
biud436 <biud436@gmail.com>
2020-10-02 19:26:00 +0900

    .

---
+++ settings.bat
@@ -0,0 +1,7 @@
+npm intall -y
+npm install -D webpack webpack-cli
+npm install --save-dev @babel/core @babel/cli
+npm install --save-dev @babel/preset-env
+npm install --save-dev style-loader css-loader
+npm install --save-dev file-loader
+powershell wget "https://github.com/biud436/InitialEditor/raw/master/webpack.config.js" -OutFile "webpack.config.js"
