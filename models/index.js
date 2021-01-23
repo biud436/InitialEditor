e00dcee34be593566a1bc08c3ac71a6d94c7f77f
biud436 <biud436@gmail.com>
2021-01-23 13:52:44 +0900

    .

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('/../config/config.json')[env];
 const db = {};
 
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
