323cb3f7ecccf003e46fe8a1126ffb9e3ae3d3f9
biud436 <biud436@gmail.com>
2021-01-23 15:22:10 +0900

    cors.

--- app.js
+++ app.js
@@ -23,12 +23,13 @@ app.set('view engine', 'ejs');
 //     console.error(err);
 //   });
 
+app.use(cors());
+app.options('*', cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
