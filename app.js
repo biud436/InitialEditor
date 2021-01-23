397f7aea80c5c2e265897950dfef8ee04fcc050a
biud436 <biud436@gmail.com>
2021-01-23 15:10:20 +0900

    added cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const { sequelize } = require("./models");
+const cors = require("cors");
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
