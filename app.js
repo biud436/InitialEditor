7f0b728d1f897ebf5250334cdecc591514cc5fc9
biud436 <biud436@gmail.com>
2021-01-23 16:44:44 +0900

    added cors()

--- app.js
+++ app.js
@@ -16,8 +16,14 @@ const timeout = require('connect-timeout')
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // sequelize.sync( {force: false} )
 //   .then(() => {
@@ -27,18 +33,14 @@ app.set('view engine', 'ejs');
 //   });
 
 app.use(timeout('5s'));
-app.use(cors());
-app.options('*', cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
