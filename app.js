e2a6bca3d7589a705fae57560156017cec057aa8
biud436 <biud436@gmail.com>
2021-01-24 16:06:47 +0900

    .

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 app.use(cookieParser());
 
+app.use("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');  
+  next();
+});
+
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ var cors = require('cors');
 
 /* GET home page. */
 router.get('/', cors(), function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');  
   res.render('index', { title: 'Initial Editor' });
 });
 
