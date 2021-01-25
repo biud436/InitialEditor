04b0df924d8c8bc48cd15372865bb4656354fa5b
biud436 <biud436@gmail.com>
2021-01-25 15:09:13 +0900

    .

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('view engine', 'ejs');
 
 // view engine setup
 app.use(express.static(__dirname + "/public"));
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // sequelize.sync( {force: false} )
 //   .then(() => {
