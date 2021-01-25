364a9b3c1a2b457abc73c86fa82b61ac4ba96b81
biud436 <biud436@gmail.com>
2021-01-25 15:30:12 +0900

    .

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const static = require("serve-static");
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
@@ -25,11 +24,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 // view engine setup
-app.use(static(path.resolve("public")));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(cors());
 
 // sequelize.sync( {force: false} )
 //   .then(() => {
@@ -51,21 +47,5 @@ app.use(cors());
 // app.use("*", todoRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
