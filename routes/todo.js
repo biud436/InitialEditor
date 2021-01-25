22a561c6ed1931a54bcc8c261084619f9ec03730
biud436 <biud436@gmail.com>
2021-01-25 15:12:06 +0900

    next()

--- routes/todo.js
+++ routes/todo.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('*', function (req, res) {
+router.get('*', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-
-    res.send('cors problem fixed:)');
+    next();
 });
 
 module.exports = router;
