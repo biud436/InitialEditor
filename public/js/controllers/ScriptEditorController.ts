30efed1900dee469638bd2ecded50c7e83eaa3d7
biud436 <biud436@gmail.com>
2021-02-05 14:07:06 +0900

    added lazy loading.

--- public/js/controllers/ScriptEditorController.ts
+++ public/js/controllers/ScriptEditorController.ts
@@ -15,19 +15,49 @@ export default class ScriptEditorController extends BaseController {
      */
     constructor(config: any) {
         super( config );
+        this.create();
+    }
+
+    /**
+     * Added react.js
+     */
+    create() {
+
+        const urls = [
+            "https://unpkg.com/react@17/umd/react.production.min.js",
+            "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"
+        ];
+
+        // 대기 횟수 계산 함수 생성
+        const pendingFunc = (i=0) => () => i++;
+        const pendingCount = pendingFunc();
+        
+        urls.forEach(src => {
+            const script = document.createElement("script");
+            script.crossOrigin = 'anonymous';
+            script.src = src;
+            script.onload = function() {
+                const isMaxCount = pendingCount() >= urls.length - 1;
+                if( isMaxCount ) {
+                    alert("React.js 스크립트 동적 바인드가 완료되었습니다.");
+                }
+            };
+
+            document.body.appendChild(script);
+        });
     }
 
     /**
      * Open the script eidtor window by using ajax.
      */
     open() {
-        const method = "/openScriptEditor";
+        const method = "/script/open";
 
         this.openWindow(method).then(result => {
             const data : ScriptEditorConfig = JSON.parse(result);
         }).catch(err => {
             console.log(err);
-        })
+        }) 
     }
 
     openWindow(url: string) : Promise<string> {
