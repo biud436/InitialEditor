54df729c52e1c66dd8cd35e62d155306b03e51a1
EoJinSeok <js.eo@harven.co.kr>
2021-08-17 18:11:09 +0900

    fixed the build.js file

--- build/build.js
+++ build/build.js
@@ -3,8 +3,15 @@ const path = require("path");
 
 let stack = [];
 
+const isUnixBase =
+    process.platform.indexOf("darwin") !== -1 ||
+    process.platform.indexOf("linux") !== -1;
+const buildCommand = isUnixBase
+    ? `npx webpack --config=webpack.config.js`
+    : `chcp 65001 | npx webpack --config=webpack.config.js`;
+
 let child = cd.exec(
-    `chcp 65001 | npx webpack --config=webpack.config.js`,
+    buildCommand,
     { cwd: path.join(__dirname, "..") },
     (err, stdout, stdin) => {
         if (err) {
@@ -13,17 +20,17 @@ let child = cd.exec(
     }
 );
 
-child.stdout.on("data", (c) => {
+child.stdout.on("data", c => {
     stack.push(c);
 });
 
-child.stdout.on("error", (c) => {
+child.stdout.on("error", c => {
     stack.push(c);
 });
 
 child.on("exit", (code, signal) => {
     console.log("빌드가 완료되었습니다.");
     cd.execSync("npm run start", {
-        cwd: path.join(__dirname, ".."),
+        cwd: path.join(__dirname, "..")
     });
 });
