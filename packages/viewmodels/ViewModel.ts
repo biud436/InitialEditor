ff747612badc52ffeb0bc9df8b32c1a6cdba47cf
biud436 <biud436@gmail.com>
2021-11-14 13:43:52 +0900

    added draggable and resizeable

--- packages/viewmodels/ViewModel.ts
+++ packages/viewmodels/ViewModel.ts
@@ -1,4 +1,4 @@
-import {EventEmitter} from "../EventEmitter";
+import { EventEmitter } from "../EventEmitter";
 import BaseController from "../controllers/BaseController";
 
 interface ViewModelImpl {
@@ -31,8 +31,14 @@ interface Status {
     history: string[];
 }
 
+declare global {
+    export interface JQuery<TElement extends HTMLElement = HTMLElement> {
+        draggable(options?: any): JQuery<TElement>;
+        resizable(options?: any): JQuery<TElement>;
+    }
+}
+
 class StatusProproties implements Status {
-    
     currentStatus: string;
     history: string[];
 
@@ -40,11 +46,10 @@ class StatusProproties implements Status {
         this.currentStatus = "NORMAL";
         this.history = [this.currentStatus];
     }
-
 }
 
-export class ViewModel extends EventEmitter implements ViewModelImpl, LifeCycle {
-
+export class ViewModel extends EventEmitter
+    implements ViewModelImpl, LifeCycle {
     protected _controller: BaseController;
     protected _element: JQuery<HTMLElement>;
     protected _status: StatusProproties;
@@ -52,43 +57,42 @@ export class ViewModel extends EventEmitter implements ViewModelImpl, LifeCycle 
     private _isReady: boolean;
 
     /**
-     * 
+     *
      */
-    constructor(__controller : BaseController) {
+    constructor(__controller: BaseController) {
         super();
-        
+
         this._isReady = false;
         this._controller = __controller;
         this._status = new StatusProproties();
 
         /**
-         * onCreate() -> 
-         * onLoad() -> 
-         * onRender() -> 
+         * onCreate() ->
+         * onLoad() ->
+         * onRender() ->
          * onShow();
          */
 
         // 라이프 싸이클과 관련된 이벤트 선언
-        this.on("create", (elem?: HTMLElement, ...args: any[]) => this.onCreate(...args))
-            .on("update", (elem?: HTMLElement)  => this.onUpdate(elem))
-            .on("stop", (elem?: HTMLElement)  => this.onStop(elem))
-            .on("dispose", (elem?: HTMLElement)  => this.onDispose(elem))
+        this.on("create", (elem?: HTMLElement, ...args: any[]) =>
+            this.onCreate(...args)
+        )
+            .on("update", (elem?: HTMLElement) => this.onUpdate(elem))
+            .on("stop", (elem?: HTMLElement) => this.onStop(elem))
+            .on("dispose", (elem?: HTMLElement) => this.onDispose(elem))
             .on("render", (result?: any) => this.onRender(result))
-            .on("show", (elem?: JQuery<HTMLElement>)  => this.onShow(elem))
+            .on("show", (elem?: JQuery<HTMLElement>) => this.onShow(elem));
 
         this.initMembers();
-
     }
 
-    initMembers() {
-
-    }
+    initMembers() {}
 
     onShow(elem?: JQuery<HTMLElement>) {
         const element = this._element;
         const controller = this._controller;
         const config = controller.config;
-        if(!element) return;
+        if (!element) return;
 
         // 화면에 창을 표시합니다.
         element.show();
@@ -99,23 +103,21 @@ export class ViewModel extends EventEmitter implements ViewModelImpl, LifeCycle 
         $(".darken, .windows-container").css("left", "0");
     }
 
-    onHide(elem?: JQuery<HTMLElement>)  {
+    onHide(elem?: JQuery<HTMLElement>) {
         const controller = this._controller;
-        const config : Config = controller.config;
+        const config: Config = controller.config;
 
         this._element.hide();
         controller.invalid();
     }
 
-    onNotify(elem?: JQuery<HTMLElement>) {
+    onNotify(elem?: JQuery<HTMLElement>) {}
 
-    }
-    
     onCreate(...args: any[]) {
         const controller = this._controller;
-        const config : Config = args[0]; 
-        
-        if(!config.parentId || !config.id) {
+        const config: Config = args[0];
+
+        if (!config.parentId || !config.id) {
             throw new Error("The parent element is not exist!");
         }
 
@@ -129,29 +131,25 @@ export class ViewModel extends EventEmitter implements ViewModelImpl, LifeCycle 
         this.onHide();
 
         // 창의 크기를 조절가능하게 만듭니다.
-        $(`#${config.id}`).resizable({containment: config.parentId});
+        $(`#${config.id}`).resizable({ containment: config.parentId });
 
         // 부모 컨테이너에 로드된 창을 추가합니다.
-        $(config.parentId).append(this._element);     
+        $(config.parentId).append(this._element);
 
-        this._isReady = true;      
+        this._isReady = true;
     }
 
     /**
      * HTML 파일로부터 도큐먼트를 렌더링합니다.
-     * @param result 
+     * @param result
      */
     onRender(result?: any) {
         this._element.html(result);
     }
 
-    onUpdate(elem?: HTMLElement, ...args: any[]) {
+    onUpdate(elem?: HTMLElement, ...args: any[]) {}
 
-    }
-
-    onStop(elem?: HTMLElement, ...args: any[]) {
-
-    }
+    onStop(elem?: HTMLElement, ...args: any[]) {}
 
     onDispose(elem?: HTMLElement, ...args: any[]) {
         this._element.fadeOut(700, () => {
@@ -159,5 +157,4 @@ export class ViewModel extends EventEmitter implements ViewModelImpl, LifeCycle 
         });
         $(".darken, .windows-container").css("left", "-9999px");
     }
-
 }
