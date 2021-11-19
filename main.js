c82f7a82e9ef175077617e674de2249f9b2cbc42
biud436 <biud436@gmail.com>
2021-11-19 13:53:43 +0900

    removed unused code.

--- main.js
+++ main.js
@@ -27,10 +27,6 @@ function createWindow() {
         win.webContents.send("change-theme");
     });
 
-    // ipcMain.on("exit", () => {
-    //     win.close();
-    // });
-
     let restoreSize = [];
 
     ipcMain.on("maximize", () => {
