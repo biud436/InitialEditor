3652e820fe2df3c0600870fe2b1e7e4f6a30e83a
biud436 <biud436@gmail.com>
2021-12-01 00:11:42 +0900

    added chalk module.

--- build/electronBuilder.js
+++ build/electronBuilder.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const fs = require("fs");
 const root = path.resolve(__dirname, "../");
+const cp = require("child_process");
+const chalk = require("chalk");
 
 fs.promises
     .copyFile(path.join(root, "index.html"), path.join(root, "dist/index.html"))
     .then(e => {
-        console.log("index.html copied");
+        console.log(chalk.yellow("index.html copied") + chalk.reset());
     })
     .catch(e => {
         console.log(e);
@@ -14,8 +16,12 @@ fs.promises
 fs.promises
     .copyFile(path.join(root, "index.js"), path.join(root, "dist/index.js"))
     .then(e => {
-        console.log("index.html copied");
+        console.log(chalk.red("index.js copied") + chalk.reset());
     })
     .catch(e => {
         console.log(e);
     });
+
+if (["darwin", "linux"].includes(process.platform)) {
+    cp.exec(`cp -r ${path.resolve(root, "src")} ${path.resolve(root, "dist")}`);
+}
---
+++ dist/src/main.ts
@@ -0,0 +1,147 @@
+const path = require("path");
+
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
+
+app.setName("InitialEditor");
+
+/**
+ * @description This object creates a configuration object for the application.
+ */
+const Config = {
+    get() {
+        // 브라우저 창을 생성합니다.
+        const isMacOS = process.platform === "darwin";
+        const windowRect = {
+            width: 1280,
+            height: 720
+        };
+        const options = {
+            ...windowRect,
+            webPreferences: {
+                enableRemoteModule: true,
+                nodeIntegration: true,
+                contextIsolation: false
+            },
+            frame: isMacOS ? true : false
+        };
+
+        return options;
+    }
+};
+
+/**
+ * @description
+ * This class allows you to create a new window that is applied some configuration.
+ */
+class MainWindow extends BrowserWindow {
+    jQuery: any;
+    $: any;
+
+    constructor(options: any) {
+        super(options);
+        this.setConfiguration();
+    }
+
+    setConfiguration() {
+        this.setMenuBarVisibility(false);
+        this.$ = this.jQuery = require("jquery");
+        this.loadURL("file://" + path.join(__dirname, "..", "index.html"));
+        this.webContents.once("dom-ready", () => {
+            this.webContents.send("change-theme");
+        });
+    }
+
+    onMaximize() {
+        let restoreSize = [];
+
+        if (!this.isMaximized()) {
+            restoreSize = this.getMaximumSize();
+            this.maximize();
+        } else {
+            this.unmaximize();
+        }
+    }
+}
+
+/**
+ * @author biud436
+ * @description
+ * This class allows you to create the electron application and start.
+ */
+class EntryPoint {
+    private _hostWindow: any;
+    constructor() {
+        this._hostWindow = null;
+    }
+
+    createWindow() {
+        this._hostWindow = new MainWindow(Config.get());
+        ipcMain.on("minimize", this._hostWindow.minimize);
+        ipcMain.on("maximize", this._hostWindow.onMaximize);
+
+        // 이렇게 하면 타입스크립트로 작성된 걸 불러와야 한다.
+        // 잘못된 구조로 짠 듯 싶다.
+        if (process.platform === "darwin") {
+            Menu.setApplicationMenu(
+                Menu.buildFromTemplate([
+                    {
+                        role: "appMenu",
+                        label: "InitialEditor",
+                        submenu: [
+                            {
+                                role: "quit"
+                            }
+                        ]
+                    },
+                    {
+                        label: "파일",
+                        role: "fileMenu",
+                        submenu: [
+                            {
+                                label: "새로 만들기",
+                                accelerator: "CmdOrCtrl+N",
+                                click: () => {
+                                    this._hostWindow.webContents.send(
+                                        "new-file"
+                                    );
+                                }
+                            },
+                            {
+                                label: "프로그램 종료",
+                                accelerator: "CmdOrCtrl+Q",
+                                role: "quit"
+                            }
+                        ]
+                    }
+                ])
+            );
+        }
+
+        return this;
+    }
+
+    listenOn() {
+        app.whenReady().then(() => {
+            this.createWindow();
+            ipcMain.emit("creatSystemMenu", this._hostWindow);
+        });
+
+        app.on("window-all-closed", () => {
+            if (process.platform !== "darwin") {
+                app.quit();
+            }
+        });
+
+        app.on("activate", () => {
+            if (BrowserWindow.getAllWindows().length === 0) {
+                this.createWindow();
+            }
+        });
+    }
+
+    static builder() {
+        return new EntryPoint();
+    }
+}
+
+EntryPoint.builder().listenOn();
