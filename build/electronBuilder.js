799f8d2f581253af802255e97470ac58a5fb994d
biud436 <biud436@gmail.com>
2021-12-08 23:08:07 +0900

    fixed electronBuilder.file

--- build/electronBuilder.js
+++ build/electronBuilder.js
@@ -1,27 +1,69 @@
 const path = require("path");
 const fs = require("fs");
-const root = path.resolve(__dirname, "../");
 const cp = require("child_process");
 const chalk = require("chalk");
 
-fs.promises
-    .copyFile(path.join(root, "index.html"), path.join(root, "dist/index.html"))
-    .then(e => {
-        console.log(chalk.yellow("index.html copied") + chalk.reset());
-    })
-    .catch(e => {
-        console.log(e);
-    });
+const FILES = {
+    HTML: {
+        INDEX: "index.html"
+    },
+    JS: {
+        INDEX: "index.js"
+    },
+    DIST: "dist"
+};
 
-fs.promises
-    .copyFile(path.join(root, "index.js"), path.join(root, "dist/index.js"))
-    .then(e => {
-        console.log(chalk.red("index.js copied") + chalk.reset());
-    })
-    .catch(e => {
-        console.log(e);
-    });
+const PLATFORMS = ["darwin", "linux"];
 
-if (["darwin", "linux"].includes(process.platform)) {
-    cp.exec(`cp -r ${path.resolve(root, "src")} ${path.resolve(root, "dist")}`);
+const MESSAGES = {};
+
+/**
+ * @class ElectronBuilder
+ */
+class ElectronBuilder {
+    constructor() {}
+
+    copyFiles() {
+        const root = path.resolve(__dirname, "../");
+
+        fs.promises
+            .copyFile(
+                path.join(root, FILES.HTML.INDEX),
+                path.join(root, FILES.DIST, FILES.HTML.INDEX)
+            )
+            .then(e => {
+                console.log(
+                    chalk.yellow(`${FILES.HTML.INDEX} copied`) + chalk.reset()
+                );
+            })
+            .catch(e => {
+                console.log(e);
+            });
+
+        fs.promises
+            .copyFile(
+                path.join(root, FILES.JS.INDEX),
+                path.join(root, FILES.DIST, FILES.JS.INDEX)
+            )
+            .then(e => {
+                console.log(
+                    chalk.red(`${FILES.JS.INDEX} copied`) + chalk.reset()
+                );
+            })
+            .catch(e => {
+                console.log(e);
+            });
+
+        if (PLATFORMS.includes(process.platform)) {
+            cp.exec(
+                `cp -r ${path.resolve(root, "src")} ${path.resolve(
+                    root,
+                    FILES.DIST
+                )}`
+            );
+        }
+    }
 }
+
+const builder = new ElectronBuilder();
+builder.copyFiles();
