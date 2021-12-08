4ba947410a05ee9055792e1d260e80b3a4fc7aae
biud436 <biud436@gmail.com>
2021-12-08 23:23:05 +0900

    added a function named "extractTypescriptDefinitionFiles"

--- build/types.js
+++ build/types.js
@@ -3,31 +3,48 @@ const path = require("path");
 const argv = require("minimist")(process.argv.slice(2));
 const cp = require("child_process");
 
-if (argv.help) {
-    console.log(`
-        node build/types.js --parent="./"
-    `);
-    process.exit(-1);
-}
+const COMMANDS = {
+    BUILD: realFileName =>
+        `cmd /k "npx typescript ${realFileName} --declaration --allowJs --emitDeclarationOnly --outDir types"`
+};
 
-let root = argv.parent || process.cwd();
-const dirs = fs.readdirSync(path.join(root, "packages", "menu"), "utf8");
-
-dirs.forEach(async (filename) => {
-    const realFileName = path.join(path.join(root, "js", "menu"), filename);
-    const command = `cmd /k "npx typescript ${realFileName} --declaration --allowJs --emitDeclarationOnly --outDir types"`;
-
-    await cp.exec(
-        command,
-        (err, stdout, stderr) => {
-            if (err) {
-                throw new Error(`${realFileName} : ${err}`);
-            }
-
-            console.log(stdout);
-        },
-        (err) => {
-            console.warn(`${realFileName} : ${err}`);
+const utils = {
+    extractTypescriptDefinitionFiles: () => {
+        if (argv.help) {
+            console.log(`
+                node build/types.js --parent="./"
+            `);
+            process.exit(-1);
         }
-    );
-});
+
+        let root = argv.parent || process.cwd();
+        const dirs = fs.readdirSync(
+            path.join(root, "packages", "menu"),
+            "utf8"
+        );
+
+        dirs.forEach(async filename => {
+            const realFileName = path.join(
+                path.join(root, "js", "menu"),
+                filename
+            );
+            const buildCommand = COMMANDS.BUILD(realFileName);
+
+            await cp.exec(
+                buildCommand,
+                (err, stdout, stderr) => {
+                    if (err) {
+                        throw new Error(`${realFileName} : ${err}`);
+                    }
+
+                    console.log(stdout);
+                },
+                err => {
+                    console.warn(`${realFileName} : ${err}`);
+                }
+            );
+        });
+    }
+};
+
+utils.extractTypescriptDefinitionFiles();
