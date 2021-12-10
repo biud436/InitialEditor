17bb292d0cf3bbc0997c0f71308c9ca6f1205a53
biud436 <biud436@gmail.com>
2021-12-10 15:57:43 +0900

    메뉴 - 그리기 란에 펜, 사각형, 원툴 동작하게 변경

--- packages/menu/DrawMenu.ts
+++ packages/menu/DrawMenu.ts
@@ -6,22 +6,30 @@ const DrawMenu = {
         "draw-pencil": {
             name: "펜",
             children: {},
-            action: (ev: any) => {}
+            action: (ev: any) => {
+                window.app.emit("tilemap:drawingType", 0);
+            }
         },
         "draw-rectangle": {
             name: "정사각형",
             children: {},
-            action: (ev: any) => {}
+            action: (ev: any) => {
+                window.app.emit("tilemap:drawingType", 1);
+            }
         },
         "draw-ellipse": {
             name: "직사각형",
             children: {},
-            action: (ev: any) => {}
+            action: (ev: any) => {
+                window.app.emit("tilemap:drawingType", 2);
+            }
         },
         "draw-flood-fill": {
             name: "채우기",
             children: {},
-            action: (ev: any) => {}
+            action: (ev: any) => {
+                window.app.emit("tilemap:drawingType", 3);
+            }
         },
         "draw-shadow pen": {
             name: "그림자",
