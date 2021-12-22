34baeba1ec36b327e48c2609ee236c33824e5cb3
biud436 <biud436@gmail.com>
2021-12-22 23:05:27 +0900

    added WebDownloader

--- packages/utils/WebRequest.ts
+++ packages/utils/WebRequest.ts
@@ -1,15 +1,19 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import * as fs from "fs";
 
 namespace InitialEditor {
     export class WebRequest {
         public static async get(
             url: string,
             config?: AxiosRequestConfig<any>
-        ): Promise<any> {
+        ): Promise<AxiosResponse<any, any>> {
             return axios.get(url, config);
         }
 
-        public static async post(url: string, data?: any): Promise<any> {
+        public static async post(
+            url: string,
+            data?: any
+        ): Promise<AxiosResponse<any, any>> {
             return axios.post(url, data);
         }
 
@@ -19,6 +23,21 @@ namespace InitialEditor {
 
         public static async delete(url: string): Promise<any> {
             return axios.delete(url);
+        }
+    }
+
+    export class WebDownloader {
+        public static async download(
+            url: string,
+            filePath: string
+        ): Promise<void> {
+            const response = await WebRequest.get(url);
+            const data = response.data;
+            const buffer = Buffer.from(data, "binary");
+            if (!Buffer.isBuffer(buffer)) {
+                return Promise.reject(new Error("Invalid data"));
+            }
+            return await fs.promises.writeFile(filePath, data);
         }
     }
 }
