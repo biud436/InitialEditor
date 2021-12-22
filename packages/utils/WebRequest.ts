bbd357c4146ba6685e11d857abc0b2165f8bd3d9
biud436 <biud436@gmail.com>
2021-12-22 22:58:45 +0900

    added put, delete

--- packages/utils/WebRequest.ts
+++ packages/utils/WebRequest.ts
@@ -12,6 +12,14 @@ namespace InitialEditor {
         public static async post(url: string, data?: any): Promise<any> {
             return axios.post(url, data);
         }
+
+        public static async put(url: string, data?: any): Promise<any> {
+            return axios.put(url, data);
+        }
+
+        public static async delete(url: string): Promise<any> {
+            return axios.delete(url);
+        }
     }
 }
 
