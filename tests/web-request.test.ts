caf77203f1265191800490538b5a0336c7757b09
biud436 <biud436@gmail.com>
2021-12-23 17:04:27 +0900

    feature2/it은 test의 alias

--- tests/web-request.test.ts
+++ tests/web-request.test.ts
@@ -1,10 +1,12 @@
 import { WebRequest } from "../packages/utils/WebRequest";
 
-it("WebRequest 유틸 테스트", () => {
+// test의 별칭이 it이다.
+// test(name, fn, timeout)
+test("WebRequest 유틸 테스트", () => {
     const response = WebRequest.get("https://www.google.com");
     response
         .then((res) => {
-            expect(res.status).toBeTruthy();
+            expect(res.status).toEqual(true);
         })
         .catch((err) => {
             console.log(err);
