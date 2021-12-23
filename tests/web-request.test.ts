a74fe595ea443f5b174de57cd14bf69e0ef81c51
biud436 <biud436@gmail.com>
2021-12-23 16:29:20 +0900

    test1/WebRequest 유틸 테스트

--- tests/web-request.test.ts
+++ tests/web-request.test.ts
@@ -1,7 +1,7 @@
 import { WebRequest } from "../packages/utils/WebRequest";
 import { jest } from "@jest/globals";
 
-it("wow", () => {
+it("WebRequest 유틸 테스트", () => {
     const response = WebRequest.get("https://www.google.com");
     expect(response).toBeTruthy();
 });
