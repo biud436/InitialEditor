29409361ab0a781fe515701d1c181938a9795609
biud436 <biud436@gmail.com>
2021-12-23 17:11:08 +0900

    단위 테스트 확인

--- tests/web-request.test.ts
+++ tests/web-request.test.ts
@@ -1,5 +1,30 @@
 import { WebRequest } from "../packages/utils/WebRequest";
 
+// beforeEach(() => {
+//     console.log("왜 어지럽지");
+// });
+// afterAll(fn, timeout)
+// afterEach(fn, timeout)
+// beforeAll(fn, timeout)
+// beforeEach(fn, timeout)
+// describe(name, fn)
+// describe.each(table)(name, fn, timeout)
+// describe.only(name, fn)
+// describe.only.each(table)(name, fn)
+// describe.skip(name, fn)
+// describe.skip.each(table)(name, fn)
+// test(name, fn, timeout)
+// test.concurrent(name, fn, timeout)
+// test.concurrent.each(table)(name, fn, timeout)
+// test.concurrent.only.each(table)(name, fn)
+// test.concurrent.skip.each(table)(name, fn)
+// test.each(table)(name, fn, timeout)
+// test.only(name, fn, timeout)
+// test.only.each(table)(name, fn)
+// test.skip(name, fn)
+// test.skip.each(table)(name, fn)
+// test.todo(name)
+
 // test의 별칭이 it이다.
 // test(name, fn, timeout)
 test("WebRequest 유틸 테스트", () => {
@@ -11,4 +36,9 @@ test("WebRequest 유틸 테스트", () => {
         .catch((err) => {
             console.log(err);
         });
+});
+
+// 모든 테스트가 끝난 후 실행됩니다.
+afterAll(() => {
+    console.log("테스트가 종료되었습니다.");
 });
