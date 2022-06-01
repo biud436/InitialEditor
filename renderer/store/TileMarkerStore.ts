081826800c9271c87a46f8f7d2a480a38dd168b9
biud436 <biud436@gmail.com>
2022-06-01 17:15:48 +0900

    feat: 컴포넌트 생성을 lazy 하게 하는 메서드 추가

--- renderer/store/TileMarkerStore.ts
+++ renderer/store/TileMarkerStore.ts
@@ -1,4 +1,5 @@
 import { makeAutoObservable } from "mobx";
+import styled, { StyledComponent } from "styled-components";
 
 export type TileMarkerState = "pending" | "ready" | "error";
 
@@ -9,12 +10,44 @@ export class TileMarkerStore {
         makeAutoObservable(this);
     }
 
-    createElement() {
+    pending() {
+        this.state = "pending";
+    }
+
+    ready() {
         this.state = "ready";
     }
 
-    removeElement() {
-        this.state = "pending";
+    /**
+     * 컴포넌트 생성을 lazy하게 합니다.
+     * (useMemo 사용 필요)
+     *
+     * @param tileWidth
+     * @param tileHeight
+     * @returns
+     */
+    createElement(
+        tileWidth: number,
+        tileHeight: number
+    ): StyledComponent<"div", any, {}, never> {
+        const lazyComponent = styled.div`
+        min-width: ${tileWidth}px;
+        min-height: ${tileHeight}px;
+        width: ${tileWidth}px;
+        height: ${tileHeight}px;
+        position: "absolute",
+        top: "0",
+        left: "0",
+        margin: "0",
+        padding: "0",
+        border: "2px dotted white",
+        z-index: "0",
+        box-sizing: "border-box",
+        `;
+
+        this.ready();
+
+        return lazyComponent;
     }
 }
 
