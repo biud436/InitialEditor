c3607c062c0ae97f7ccaad44d0a36abbcc4821f8
biud436 <biud436@gmail.com>
2022-08-08 23:15:33 +0900

    fix: added key

--- renderer/components/menu/menuItems/DrawMenuItem.tsx
+++ renderer/components/menu/menuItems/DrawMenuItem.tsx
@@ -5,27 +5,30 @@ import { ListItem } from "@components/atomics/ListItem";
 export function DrawMenuitem() {
     return (
         // ul을 컴포넌트로 감싸면 유니크 클래스 명이 생성되는 듯 하다.
-        <ul className="menu__draw-sub menu-style" defaultValue="Draw">
-            <ListItem data-action="draw-pencil">
+        <ListContainer
+            className="menu__draw-sub menu-style"
+            defaultValue="Draw"
+        >
+            <ListItem data-action="draw-pencil" key="draw-pencil_x1r1">
                 <IconItem className="fas fa-pencil-alt" />
                 Pencil
             </ListItem>
-            <ListItem data-action="draw-rectangle">
+            <ListItem data-action="draw-rectangle" key="draw-rectangle_x1r1">
                 <IconItem className="fas fa-square-full" />
                 Rectangle
             </ListItem>
-            <ListItem data-action="draw-ellipse">
+            <ListItem data-action="draw-ellipse" key="draw-ellipse_x2r2">
                 <IconItem className="fas fa-circle" />
                 Ellipse
             </ListItem>
-            <ListItem data-action="draw-flood-fill">
+            <ListItem data-action="draw-flood-fill" key="draw-flood-fill_x2r2">
                 <IconItem className="fas fa-fill" />
                 Flood Fill
             </ListItem>
-            <ListItem data-action="draw-shadow-pen">
+            <ListItem data-action="draw-shadow-pen" key="draw-shadow-pen_x3r3">
                 <IconItem className="fas fa-paint-brush" />
                 Shadow Pen
             </ListItem>
-        </ul>
+        </ListContainer>
     );
 }
