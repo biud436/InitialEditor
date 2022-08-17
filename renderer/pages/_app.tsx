abdbf46f7e14cede6174e2eae7e0f3018bdc43e0
biud436 <biud436@gmail.com>
2022-08-17 18:23:53 +0900

    build: added a new module that can inject other class

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import React from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.scss";
@@ -5,13 +6,13 @@ import { ThemeProvider } from "styled-components";
 import { RecoilRoot } from "recoil";
 
 export function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <RecoilRoot>
-            <ThemeProvider theme={{}}>
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </RecoilRoot>
-    );
+  return (
+    <RecoilRoot>
+      <ThemeProvider theme={{}}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
