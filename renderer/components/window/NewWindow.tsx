5185368d54f8ec03d60d59a21eb37677c53a62b8
biud436 <biud436@gmail.com>
2022-08-17 13:13:09 +0900

    fix: fixed issue that is not working on Windows due to i18n translate plugin

--- renderer/components/window/NewWindow.tsx
+++ renderer/components/window/NewWindow.tsx
@@ -1,7 +1,6 @@
 import { useMemo, useState } from "react";
 import { useClose as useClose } from "../../providers/window.providers";
 import { BaseWindowFrame } from "./BaseWindowFrame";
-import useTranslation from "next-translate/useTranslation";
 import styled from "styled-components";
 import { Division } from "@components/atomics/Wrapper";
 import { ListContainer } from "@components/atomics/ListContainer";
@@ -14,77 +13,76 @@ import { Span } from "@components/atomics/Span";
 import { IconItem } from "@components/atomics/IconItem";
 
 export type Project = {
-    path: FileList;
-    author: string;
+  path: FileList;
+  author: string;
 };
 
 export type ReactWindowProps = { children: React.ReactNode };
 
 export default function NewWindow({ children }: ReactWindowProps) {
-    const { close } = useClose();
-    const [gameName, setGameName] = useState("");
-    const [, setFileName] = useState("");
-    const { t } = useTranslation("common");
+  const { close } = useClose();
+  const [gameName, setGameName] = useState("");
+  const [, setFileName] = useState("");
 
-    const windowRect = useMemo(() => {
-        return {
-            width: 256,
-            height: 256,
-        };
-    }, []) as { width: number; height: number };
-
-    const onChangeGameName = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setGameName(e.target.value);
+  const windowRect = useMemo(() => {
+    return {
+      width: 256,
+      height: 256,
     };
+  }, []) as { width: number; height: number };
 
-    const onChangeFileName = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFileName(e.target.value);
-    };
+  const onChangeGameName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGameName(e.target.value);
+  };
 
-    return (
-        <BaseWindowFrame props={windowRect}>
-            <Division className="newContainer">
-                <Division id="newWindow" window-name={t("gameProperty")}>
-                    <ListContainer>
-                        <ListItem key="583334c93fe9fa509d811fc0b2928056_gameName">
-                            <Label htmlFor="name">{t("gameName")}</Label>
-                            <Input
-                                type="text"
-                                placeholder="name"
-                                value={gameName}
-                                onChange={onChangeGameName}
-                            />
-                        </ListItem>
-                        <ListItem key="583334c93fe9fa509d811fc0b2928056_gameFile">
-                            <Label htmlFor="name">{t("gameFile")}</Label>
-                            <Input
-                                type="file"
-                                placeholder=""
-                                multiple
-                                accept="directory"
-                                onChange={onChangeFileName}
-                            />
-                        </ListItem>
-                    </ListContainer>
-                    <Division className="newWindow__control-box">
-                        <Paragraph>
-                            <Span>
-                                <IconItem
-                                    className="far fa-window-close"
-                                    id="action-close"
-                                    onClick={close}
-                                ></IconItem>
-                            </Span>
-                        </Paragraph>
-                    </Division>
-                    <Division className="panel">
-                        <Button>
-                            <i className="fas fa-upload"></i>
-                            {t("newGame")}
-                        </Button>
-                    </Division>
-                </Division>
-            </Division>
-        </BaseWindowFrame>
-    );
+  const onChangeFileName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFileName(e.target.value);
+  };
+
+  return (
+    <BaseWindowFrame props={windowRect}>
+      <Division className="newContainer">
+        <Division id="newWindow" window-name={"gameProperty"}>
+          <ListContainer>
+            <ListItem key="583334c93fe9fa509d811fc0b2928056_gameName">
+              <Label htmlFor="name">{gameName}</Label>
+              <Input
+                type="text"
+                placeholder="name"
+                value={gameName}
+                onChange={onChangeGameName}
+              />
+            </ListItem>
+            <ListItem key="583334c93fe9fa509d811fc0b2928056_gameFile">
+              <Label htmlFor="name">{"gameFile"}</Label>
+              <Input
+                type="file"
+                placeholder=""
+                multiple
+                accept="directory"
+                onChange={onChangeFileName}
+              />
+            </ListItem>
+          </ListContainer>
+          <Division className="newWindow__control-box">
+            <Paragraph>
+              <Span>
+                <IconItem
+                  className="far fa-window-close"
+                  id="action-close"
+                  onClick={close}
+                ></IconItem>
+              </Span>
+            </Paragraph>
+          </Division>
+          <Division className="panel">
+            <Button>
+              <i className="fas fa-upload"></i>
+              {"newGame"}
+            </Button>
+          </Division>
+        </Division>
+      </Division>
+    </BaseWindowFrame>
+  );
 }
--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,28 +1,49 @@
+// const path = require("path");
+// const nextTranslate = require("next-translate");
+
+// /**
+//  * @type {import('next/config').NextConfig}
+//  */
+// module.exports = nextTranslate({
+//   /**
+//    * @param {import('next').NextConfig} config
+//    **/
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.target = "electron-renderer";
+//       config.node = {
+//         __dirname: true,
+//       };
+
+//       // config.resolve.alias = {
+//       //     "@components": path.resolve(__dirname, "components"),
+//       // };
+//     }
+
+//     return config;
+//   },
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, "styles")],
+//   },
+// });
+
 const path = require("path");
-const nextTranslate = require("next-translate");
 
-/**
- * @type {import('next/config').NextConfig}
- */
-module.exports = nextTranslate({
-    /**
-     * @param {import('next').NextConfig} config
-     **/
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.target = "electron-renderer";
-            config.node = {
-                __dirname: true,
-            };
+module.exports = {
+  /**
+   * @param {import('next').NextConfig} config
+   **/
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.target = "electron-renderer";
+      config.node = {
+        __dirname: true,
+      };
+    }
 
-            // config.resolve.alias = {
-            //     "@components": path.resolve(__dirname, "components"),
-            // };
-        }
-
-        return config;
-    },
-    sassOptions: {
-        includePaths: [path.join(__dirname, "styles")],
-    },
-});
+    return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
