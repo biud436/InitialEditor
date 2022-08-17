b2b56bbb0cd01c51a7c06c2067080155e1664a68
biud436 <biud436@gmail.com>
2022-08-17 18:23:53 +0900

    build: added a new module that can inject other class

--- renderer/components/frame/Tilemap.tsx
+++ renderer/components/frame/Tilemap.tsx
@@ -2,11 +2,11 @@ import { Canvas } from "../atomics/Canvas";
 import { Division } from "../atomics/Wrapper";
 
 export function Tilemap() {
-    return (
-        <Division className="contents">
-            <Canvas id="contents__main-canvas">
-                캔버스를 지원하지 않는 기기입니다.
-            </Canvas>
-        </Division>
-    );
+  return (
+    <Division className="contents">
+      <Canvas id="contents__main-canvas">
+        Your browser can not support canvas tag.
+      </Canvas>
+    </Division>
+  );
 }
--- renderer/components/menu/menuItems/TopDownNavigationMenu.tsx
+++ renderer/components/menu/menuItems/TopDownNavigationMenu.tsx
@@ -4,60 +4,60 @@ import { ListContainer } from "@components/atomics/ListContainer";
 import { ListItem } from "@components/atomics/ListItem";
 
 export function TopDownNavigationMenu() {
-    return (
-        <ListContainer className="menu__main">
-            <ListItem className="menu__main-program-icon"></ListItem>
-            <ListItem className="menu__main-file">
-                <Label htmlFor="file" data-action="file">
-                    파일
-                </Label>
-            </ListItem>
-            <ListItem>
-                <Label htmlFor="edit" data-action="edit">
-                    Edit
-                </Label>
-            </ListItem>
-            <ListItem>
-                <Label htmlFor="mode" data-action="mode">
-                    Mode
-                </Label>
-            </ListItem>
-            <ListItem>
-                <Label htmlFor="draw" data-action="draw">
-                    Draw
-                </Label>
-            </ListItem>
-            <ListItem>
-                <Label htmlFor="scale" data-action="scale">
-                    Scale
-                </Label>
-            </ListItem>
-            <ListItem>
-                <Label htmlFor="tools" data-action="tools">
-                    Tools
-                </Label>
-            </ListItem>
-            <ListItem>
-                <Label htmlFor="game" data-action="game">
-                    Game
-                </Label>
-            </ListItem>
-            <ListItem>
-                <Label htmlFor="help" data-action="help">
-                    Help
-                </Label>
-            </ListItem>
-            <ListContainer className="control-box">
-                <ListItem className="minimum" data-action="minimum">
-                    <IconItem className="fas fa-minus"></IconItem>
-                </ListItem>
-                <ListItem className="maximum" data-action="maximum">
-                    <IconItem className="fas fa-window-maximize"></IconItem>
-                </ListItem>
-                <ListItem className="close" data-action="close">
-                    <IconItem className="far fa-window-close"></IconItem>
-                </ListItem>
-            </ListContainer>
-        </ListContainer>
-    );
+  return (
+    <ListContainer className="menu__main">
+      <ListItem className="menu__main-program-icon"></ListItem>
+      <ListItem className="menu__main-file">
+        <Label htmlFor="file" data-action="file">
+          File
+        </Label>
+      </ListItem>
+      <ListItem>
+        <Label htmlFor="edit" data-action="edit">
+          Edit
+        </Label>
+      </ListItem>
+      <ListItem>
+        <Label htmlFor="mode" data-action="mode">
+          Mode
+        </Label>
+      </ListItem>
+      <ListItem>
+        <Label htmlFor="draw" data-action="draw">
+          Draw
+        </Label>
+      </ListItem>
+      <ListItem>
+        <Label htmlFor="scale" data-action="scale">
+          Scale
+        </Label>
+      </ListItem>
+      <ListItem>
+        <Label htmlFor="tools" data-action="tools">
+          Tools
+        </Label>
+      </ListItem>
+      <ListItem>
+        <Label htmlFor="game" data-action="game">
+          Game
+        </Label>
+      </ListItem>
+      <ListItem>
+        <Label htmlFor="help" data-action="help">
+          Help
+        </Label>
+      </ListItem>
+      <ListContainer className="control-box">
+        <ListItem className="minimum" data-action="minimum">
+          <IconItem className="fas fa-minus"></IconItem>
+        </ListItem>
+        <ListItem className="maximum" data-action="maximum">
+          <IconItem className="fas fa-window-maximize"></IconItem>
+        </ListItem>
+        <ListItem className="close" data-action="close">
+          <IconItem className="far fa-window-close"></IconItem>
+        </ListItem>
+      </ListContainer>
+    </ListContainer>
+  );
 }
