40a6092b419cd37ccac188204cf6f626b896ca11
biud436 <biud436@gmail.com>
2022-08-19 13:42:08 +0900

    feat: 타일셋 선택창을 컴포넌트로 1차적으로 분리 및 테스트

--- renderer/components/frame/index.tsx
+++ renderer/components/frame/index.tsx
@@ -1,5 +1,6 @@
 import { Tilemap } from "./Tilemap";
 import { TileSelectWindow } from "./TileSelectWindow";
 import { Toolbar } from "./Toolbar";
+import { TilesetMarker } from "./TilesetMarker";
 
-export { Tilemap, TileSelectWindow, Toolbar };
+export { Tilemap, TileSelectWindow, Toolbar, TilesetMarker };
