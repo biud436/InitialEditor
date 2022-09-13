e42b555eceda5a32d763722555a3fcaf48eab2f4
biud436 <biud436@gmail.com>
2022-09-13 16:54:08 +0900

    feat: added a new interface

--- packages/menu/MeatadataStorage.ts
+++ packages/menu/MeatadataStorage.ts
@@ -1,5 +1,6 @@
 import Container, { Service } from "typedi";
 import { IBaseMenuCommand } from "./commands/IBaseMenuCommand";
+import { IMenuItem, MenuType } from "./KoreanMenu";
 
 export type MenuCommandTarget = {
   target: IBaseMenuCommand;
@@ -15,12 +16,43 @@ export interface MenuAction {
   action<T = any>(ev: T): void;
 }
 
+export type Optional<T> = {
+  [P in keyof T]?: T[P];
+};
+
+/**
+ * @class MetadataStorage
+ */
 @Service()
 export class MetadataStorage {
   menuCommands: MenuCommandTarget[] = [];
   menuActions: MenuAction[] = [];
+
+  getCommand(name: string): MenuCommandTarget | undefined {
+    return this.menuCommands.find((command) => command.name === name);
+  }
+
+  getCommandItems(menuId: MenuType) {
+    const command = this.getCommand(menuId);
+    if (!command) {
+      console.warn(`command named '${menuId}' not found`);
+      return;
+    }
+
+    const ret: Optional<IMenuItem> = {
+      ...command,
+      name: menuId,
+    };
+
+    return ret;
+  }
 }
 
+/**
+ * Getting the metadata storage from the global scope.
+ *
+ * @returns {MetadataStorage} MetadataStorage
+ */
 export function getMetadataStorage() {
   return Container.get(MetadataStorage);
 }
--- renderer/types/menu/KoreanMenu.d.ts
+++ renderer/types/menu/KoreanMenu.d.ts
@@ -1,13 +1,19 @@
-export declare type MenuType = "file" | "edit" | "mode" | "draw" | "scale" | "tools" | "game" | "help";
-declare const KoreanMenu: {
-    file: Partial<import("./FileMenu").FileMenuImpl>;
-    edit: Partial<import("./EditMenu").EditMenuImpl>;
-    mode: Partial<import("./ModeMenu").ModeMenuImpl>;
-    draw: Partial<import("./DrawMenu").DrawMenuImpl>;
-    scale: Partial<import("./ScaleMenu").ScaleMenuImpl>;
-    tools: Partial<import("./ToolMenu").ToolMenuImpl>;
-    game: Partial<import("./GameMenu").GameMenuImpl>;
-    help: Partial<import("./HelpMenu").HelpMenuImpl>;
+import { Optional } from "./MeatadataStorage";
+export declare type IMenuParent = {
+    [key in MenuType]: Optional<IMenuItem>;
 };
-export declare type MenuKeys = keyof typeof KoreanMenu;
+export interface IMenuItem {
+    name: string;
+    children: {
+        [key: string]: {
+            name?: string;
+            children?: Partial<Record<string, any>>;
+            shortcut?: string[];
+            action?: (ev: any) => void | Function;
+        };
+    };
+}
+export declare type MenuType = "file" | "edit" | "mode" | "draw" | "scale" | "tools" | "game" | "help";
+export declare type MenuKeys = keyof IMenuParent;
+declare const KoreanMenu: IMenuParent;
 export { KoreanMenu };
