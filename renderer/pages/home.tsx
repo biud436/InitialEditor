29a9d9addc80da0d8309b8b4fb9abfa8d308e531
biud436 <biud436@gmail.com>
2022-09-13 18:12:23 +0900

    chore: added monaco editor

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -9,48 +9,48 @@ import Widget from "../components/window/Widget";
 import styled from "styled-components";
 import { observer } from "mobx-react";
 
+// test
+import { useRouter } from "next/router";
+
 const LoadingBlock = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Home = observer(() => {
-    const [panel, setPanel] = useRecoilState(WindowState);
-    const [loading, setLoading] = useState(true);
+  const [panel, setPanel] = useRecoilState(WindowState);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        window.$ = jQuery;
-        window.onMounted(() => {
-            if (window.app) {
-                window.app.on("openWindow", openWindow);
-            }
-        });
-    }, []);
+  useEffect(() => {
+    window.$ = jQuery;
+    window.onMounted(() => {
+      if (window.app) {
+        window.app.on("openWindow", openWindow);
+      }
+    });
+  }, []);
 
-    const openWindow = ({ path }: { path: string }) => {
-        const windowName = path.slice(1);
-        setPanel({
-            currentWindow: windowName as WindowType,
-        });
-    };
+  const openWindow = ({ path }: { path: string }) => {
+    const windowName = path.slice(1);
+    setPanel({
+      currentWindow: windowName as WindowType,
+    });
+  };
 
-    return (
-        <React.Fragment>
-            <Head>
-                <meta charSet="UTF-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-                <title></title>
-            </Head>
-            <MainContainer />
-            <Widget />
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title></title>
+      </Head>
+      <MainContainer />
+      <Widget />
+    </React.Fragment>
+  );
 });
 
 export default Home;
