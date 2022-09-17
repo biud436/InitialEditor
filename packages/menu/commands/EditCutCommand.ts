6d4319159d023b3b835c96b71ed41c59c964ebc1
biud436 <biud436@gmail.com>
2022-09-17 20:42:45 +0900

    feat: 편집 메뉴의 커맨드 추가

---
+++ packages/menu/commands/EditCutCommand.ts
@@ -0,0 +1,15 @@
+import { ElectronService } from "../../ElectronService";
+import { MenuCommand } from "../../decorators/MenuCommand";
+import { OnMenuClick } from "../../decorators/OnMenuClick";
+import { IBaseMenuCommand } from "./IBaseMenuCommand";
+
+@MenuCommand("edit", "edit-cut", "잘라내기", ["ctrl", "x"])
+export class EditCutCommand implements IBaseMenuCommand {
+    @OnMenuClick("edit-cut")
+    action(ev: any): void {
+        ElectronService.getInstance().showErrorMessageBox(
+            "알림",
+            "잘라내기 기능을 실행하였습니다."
+        );
+    }
+}
---
+++ renderer/types/menu/commands/EditUndoCommand.d.ts
@@ -0,0 +1,4 @@
+import { IBaseMenuCommand } from "./IBaseMenuCommand";
+export declare class EditUndoCommand implements IBaseMenuCommand {
+    action(ev: any): void;
+}
